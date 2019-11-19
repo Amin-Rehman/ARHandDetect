bc5c270174292f73dde20f7e1d710d6d0209647f
Amin Rehman <amrehman@microsoft.com>
2019-11-19 14:48:49 +0100

    Add the first dev tests.

--- ARHandDev/ARHandDevTests/ARHandDevTests.swift
+++ ARHandDev/ARHandDevTests/ARHandDevTests.swift
@@ -7,6 +7,7 @@
 //
 
 import XCTest
+@testable import ARHandDev
 
 class ARHandDevTests: XCTestCase {
 
@@ -19,15 +20,6 @@ class ARHandDevTests: XCTestCase {
     }
 
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
     }
 
 }
