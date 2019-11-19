54de76bbf64de28e75bf41314672e7f64a3ab1c8
Amin Rehman <amrehman@microsoft.com>
2019-11-19 14:43:58 +0100

    Moving some stuff around

---
+++ ARHandDev/ARHandDev/Utilities/CoordinateMaker.swift
@@ -0,0 +1,18 @@
+//
+//  CoordinateMaker.swift
+//  ARHandDev
+//
+//  Created by Amin Rehman on 19.11.19.
+//  Copyright © 2019 Stephanie. All rights reserved.
+//
+
+import Foundation
+
+struct Coordinates {
+    let x: Float
+    let y: Float
+}
+
+struct CoordinateMaker {
+
+}
